541583e4e67e2f3a87c3c0292a939ce32cb980ed
JhonardVictorillo <victorillojhonard@gmail.com>
2024-06-19 00:57:59 -0700

    new update edited

--- create.php
+++ create.php
@@ -130,7 +130,7 @@ include_once("connect.php");
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
 
-  <body>
+  <body class="createcss">
     <h1>ADD PATIENT RECORD</h1>
 
       <?php if(!empty($errors)):?>
